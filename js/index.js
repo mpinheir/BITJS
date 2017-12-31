992cafb8c224d65fac5f2a72d59487a1995eac8f
Hamon Córdova <hamoncj@gmail.com>
2017-12-31 10:02:24 -0200

    Add BITCOIN value in BRL

--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@ var xmlhttp = new XMLHttpRequest();
 xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
         var myObj = JSON.parse(this.responseText);
-        
+
         var usrate = myObj.bpi.USD.rate;
         var gbrate = myObj.bpi.GBP.rate;
         var eurrate = myObj.bpi.EUR.rate;
@@ -13,10 +13,33 @@ xmlhttp.onreadystatechange = function() {
         document.querySelector('.uscurrency span').innerHTML = "$ "+usrate;
         document.querySelector('.gbpcurrency span').innerHTML = "£ "+gbrate;
         document.querySelector('.eurcurrency span').innerHTML = "€ "+eurrate;
-        
+
+        //Get Bitcoin value in R$ (BRL)
+        getBrlRate(usrate);
+
     }
 }
 
 xmlhttp.open('GET', 'https://api.coindesk.com/v1/bpi/currentprice.json', true);
 xmlhttp.send();
 
+function getBrlRate () {
+
+  var btcToBrlXmlHttp = new XMLHttpRequest();
+
+  btcToBrlXmlHttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+
+      var brlBitCoinObj = JSON.parse(this.responseText);
+
+      //Get BRL rate and display in table
+      var brlRate = brlBitCoinObj.ticker.sell;
+      document.querySelector('.brlcurrency span').innerHTML = "$ "+ brlRate;
+
+    }
+  }
+
+  btcToBrlXmlHttp.open('GET', 'https://www.mercadobitcoin.net/api/BTC/ticker/', true)
+  btcToBrlXmlHttp.send();
+
+}
